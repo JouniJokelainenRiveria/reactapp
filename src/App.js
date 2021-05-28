ab433c3a6b1782a8681dcd0a657250a6d3a03083
Jouni Jokelainen <jouni.jokelainen@riveria.fi>
2021-05-28 11:11:48 +0300

    sovellusta muokattu

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Terve vaan!
+          Well..Hello again!!
         </p>
         <a
           className="App-link"
